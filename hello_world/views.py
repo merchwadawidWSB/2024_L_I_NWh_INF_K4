dfed667ce3b688a94a8d6ba735f059a3c73d03d3
merchwadawidWSB <dmerchwa@gmail.com>
2024-04-13 15:46:23 +0200

    Usuniecie bledu z aplikacji

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Dawide"
+moje_imie = "Dawid"
 msg = "Hello World!"
 
 
